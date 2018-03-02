acd48cbcd7537a436a9b928bd827fc4489c93971
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-03-02 11:28:27 -0500

    Update files for new delta-client package

---
+++ static/js/example/index.ts
@@ -0,0 +1,7 @@
+import * as Delta from "node_modules/@xroadsed/delta-client/index";
+
+export class IndexComponent extends Delta.Component {
+
+}
+
+export default IndexComponent
---
+++ static/js/example/other.ts
@@ -0,0 +1,7 @@
+import * as Delta from "node_modules/@xroadsed/delta-client/index";
+
+export class OtherComponent extends Delta.Component {
+
+}
+
+export default OtherComponent
